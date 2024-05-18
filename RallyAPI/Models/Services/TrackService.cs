55097fa4aa77baeb70be098d6cbbf505e64f252a
Aske Lysgaard <askelysgaard@gmail.com>
2024-05-18 13:02:53 +0200

    safsaf

--- RallyAPI/Models/Services/TrackService.cs
+++ RallyAPI/Models/Services/TrackService.cs
@@ -5,69 +5,69 @@ namespace RallyAPI.Models.Services;
 
 public class TrackService
 {
-    private readonly IMongoCollection<Track> _tracks;
+	private readonly IMongoCollection<Track> _tracks;
 
-    public TrackService(IDbAccess<Track> dbAccess)
-    {
-        _tracks = dbAccess.GetDbClient("Track");
-    }
+	public TrackService(IDbAccess<Track> dbAccess)
+	{
+		_tracks = dbAccess.GetDbClient("Track");
+	}
 
-    public Track GetOne(string userRole, string userName, string name = "", string date = "", string location = "", string theme = "")
-    {
-        var claimsQuery = _tracks.AsQueryable().Where(t => t.RoleClaims!.Contains(userRole) ||
-                                                      t.UserClaims!.Contains(userName));
+	public Track GetOne(string userRole, string userName, string name = "", string date = "", string location = "", string theme = "")
+	{
+		var claimsQuery = _tracks.AsQueryable().Where(t => t.RoleClaims!.Contains(userRole) ||
+													  t.UserClaims!.Contains(userName));
 
-        var searchQuery = claimsQuery.Where(t => t.Name == name ||
-                                                t.Date.ToString() == date ||
-                                                t.Location == location ||
-                                                t.Theme == theme).FirstOrDefault();
-        try
-        {
-            var id = searchQuery.Id;
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-            return null;
-        }
-        return searchQuery;
-    }
+		var searchQuery = claimsQuery.Where(t => t.Name == name ||
+												t.Date.ToString() == date ||
+												t.Location == location ||
+												t.Theme == theme).FirstOrDefault();
+		try
+		{
+			var id = searchQuery.Id;
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.Message);
+			return null;
+		}
+		return searchQuery;
+	}
 
-    public IEnumerable<Track> GetAll(string userRole, string userName)
-    {
-        var query = _tracks.AsQueryable().Where(t => t.RoleClaims!.Contains(userRole) || t.UserClaims!.Contains(userName));
+	public IEnumerable<Track> GetAll(string userRole, string userName)
+	{
+		var query = _tracks.AsQueryable().Where(t => t.RoleClaims!.Contains(userRole) || t.UserClaims!.Contains(userName));
 
-        return query;
-    }
+		return query;
+	}
 
-    public Track SaveNewTrack(Track track)
-    {
-        _tracks.InsertOne(track);
+	public Track SaveNewTrack(Track track)
+	{
+		_tracks.InsertOne(track);
 
-        var query = _tracks.AsQueryable().Where(t => t.Date == track.Date).FirstOrDefault();
+		var query = _tracks.AsQueryable().Where(t => t.Date == track.Date).FirstOrDefault();
 
-        return query;
-    }
+		return query;
+	}
 
-    public Track UpdateTrack(Track track)
-    {
-        track.Id = _tracks.AsQueryable().Where(t => t.Name == track.Name && t.Date == track.Date).FirstOrDefault()!.Id;
+	public Track UpdateTrack(Track track)
+	{
+		track.Id = _tracks.AsQueryable().Where(t => t.Name == track.Name && t.Date == track.Date).FirstOrDefault()!.Id;
 
-        var filter = Builders<Track>.Filter.Eq(t => t.Id, track.Id);
+		var filter = Builders<Track>.Filter.Eq(t => t.Id, track.Id);
 
-        _tracks.ReplaceOne(filter, track);
+		_tracks.ReplaceOne(filter, track);
 
-        return _tracks.Find(filter).FirstOrDefault();
-    }
+		return _tracks.Find(filter).FirstOrDefault();
+	}
 
-    public bool DeleteTrack(Track track)
-    {
-        track.Id = _tracks.AsQueryable().Where(t => t.Name == track.Name && t.Date == track.Date).FirstOrDefault()!.Id;
+	public bool DeleteTrack(Track track)
+	{
+		track.Id = _tracks.AsQueryable().Where(t => t.Name == track.Name && t.Date == track.Date).FirstOrDefault()!.Id;
 
-        var filter = Builders<Track>.Filter.Eq(t => t.Id, track.Id);
+		var filter = Builders<Track>.Filter.Eq(t => t.Id, track.Id);
 
-        var result = _tracks.DeleteOne(filter);
+		var result = _tracks.DeleteOne(filter);
 
-        return result.IsAcknowledged;
-    }
+		return result.IsAcknowledged;
+	}
 }
